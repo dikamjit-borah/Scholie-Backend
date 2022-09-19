b1d15875e00f58effe4a411016c00f65573f7b56
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-09-19 20:47:56 +0530

    transaction to insert into assignments (tutors & students)

--- controllers/controller.tutor.js
+++ controllers/controller.tutor.js
@@ -9,26 +9,36 @@ const constants = require("../utils/constants")
 module.exports = {
     assignmentCreate: async function (req, res) {
         basicUtils.logger(TAG, `Requesting ${req.url}`)
-        const {
-            assignmentDescription,
-            assignmentPublishedAt,
-            assignmentDeadline,
-            assignmentStudents } = { ...req.body }
-
-        const assignmentId = v4()
-
         try {
-            const result = await serviceTutor.createNewAssignment(
-                assignmentId,
-                'assignmentDescription',
-                new Date(),
-                new Date(),
-                [1, 2, 3])
+            const {
+                assignmentDescription,
+                assignmentPublishedAt,
+                assignmentDeadline,
+                assignmentStudents } = { ...req.body }
 
-            if (result && result.length) {
-                if (result[0]) return basicUtils.generateResponse(res, httpStatus.INTERNAL_SERVER_ERROR, constants.messages.ASSIGNMENT_CREATE_ERR, { error: "" + result[0] })
-                if (result[1]) return basicUtils.generateResponse(res, httpStatus.OK, constants.messages.ASSIGNMENT_CREATE_SUCCESS)
+            const assignmentId = v4()
+            let assignmentsStudentsRows = []
+
+            if (assignmentStudents && Array.isArray(assignmentStudents) && assignmentStudents.length) {
+                assignmentsStudentsRows = assignmentStudents.map((studentId) => {
+                    return Object.values({ assignmentId, studentId })
+                })
             }
+
+            const result1 = await serviceTutor.createNewAssignment(
+                assignmentId,
+                assignmentDescription,
+                assignmentPublishedAt,
+                assignmentDeadline, 
+                assignmentsStudentsRows
+            )
+
+
+            if (result1 && result1.length) {
+                if (result1[0]) return basicUtils.generateResponse(res, httpStatus.INTERNAL_SERVER_ERROR, constants.messages.ASSIGNMENT_CREATE_ERR, { error: "" + result1[0] })
+                if (result1[1]) return basicUtils.generateResponse(res, httpStatus.OK, constants.messages.ASSIGNMENT_CREATE_SUCCESS)
+            }
+
 
         } catch (error) {
             return basicUtils.generateResponse(res, httpStatus.INTERNAL_SERVER_ERROR, constants.messages.ASSIGNMENT_CREATE_ERR, { error: "" + error })
--- services/service.tutor.js
+++ services/service.tutor.js
@@ -1,7 +1,27 @@
 const db = require('../models/index').sequelize
 
 module.exports = {
+
     createNewAssignment: async function (...args) {
+        let err
+        let isCreated = false
+        try {
+            const query = `START TRANSACTION; INSERT INTO assignments_tutors (assignmentId, assignmentDescription, assignmentPublishedAt, assignmentDeadline) VALUES (?, ?, ?, ?); INSERT INTO assignments_students (entryId, assignmentId, studentId) VALUES ?; COMMIT;`
+            let result = await db.query(query, {
+                replacements: args,
+                logging: console.log
+            })
+
+            isCreated = result && result.length ? true : false
+        } catch (error) {
+            console.log(error)
+            isCreated = false
+        }
+
+        return [err, isCreated]
+    },
+
+    insertIntoAssignmentsTutors: async function (...args) {
         let err
         let isCreated = false
         try {
@@ -10,7 +30,7 @@ module.exports = {
                 replacements: args,
                 logging: console.log
             })
-            
+
             isCreated = result && result.length ? true : false
         } catch (error) {
             console.log(error)
@@ -18,5 +38,24 @@ module.exports = {
         }
 
         return [err, isCreated]
-    }
+    },
+
+    insertIntoAssignmentsStudents: async function (rows) {
+        let err
+        let isCreated = false
+        try {
+            const query = `INSERT INTO assignments_students (entry_id, assignment_id, student_id) VALUES ?`
+            let result = await db.query(query, {
+                replacements: rows,
+                logging: console.log
+            })
+
+            isCreated = result && result.length ? true : false
+        } catch (error) {
+            console.log(error)
+            isCreated = false
+        }
+
+        return [err, isCreated]
+    },
 }

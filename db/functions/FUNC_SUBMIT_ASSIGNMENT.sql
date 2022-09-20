2b258ec64b2c39ef236d1c1848f4bc9593b465c5
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-09-20 18:29:43 +0530

    mysql function to handle assignment submission

---
+++ db/functions/FUNC_SUBMIT_ASSIGNMENT.sql
@@ -0,0 +1,22 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `FUNC_SUBMIT_ASSIGNMENT`(p_studentId integer,
+p_assignmentId varchar(255),
+p_assignmentRemark varchar(255)) RETURNS varchar(255) CHARSET utf8mb4
+BEGIN   
+	DECLARE d_entryId integer;
+    DECLARE d_assignmentStatus integer;
+    
+SELECT 
+    entryId, assignmentStatus
+INTO d_entryId , d_assignmentStatus FROM
+    assignments_students
+WHERE
+    assignmentId = p_assignmentId
+        && studentId = p_studentId;
+	IF ISNULL(d_entryId) THEN RETURN 'Details not found for the given assignment or student id';  
+	ELSE 
+		IF NOT ISNULL(d_assignmentStatus) THEN RETURN 'Assignment has already been submitted';  
+        ELSE UPDATE assignments_students SET assignmentRemark = p_assignmentRemark, assignmentStatus = 1  WHERE assignmentId = p_assignmentId AND studentId = p_studentId;
+        RETURN 'Assignment successfully submitted'; 
+	END IF;
+    END IF;
+END
